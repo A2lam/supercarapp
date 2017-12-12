a3f8e142183e0fe624703d088b9e9ff121bf20bd
A2lam <mohamedallam31@hotmail.com>
2017-12-12 07:27:49 +0100

    Raccord Seller Storehouse

--- src/A2/UserBundle/Entity/Seller.php
+++ src/A2/UserBundle/Entity/Seller.php
@@ -23,7 +23,19 @@ class Seller
     private $id;
 
     /**
+     * @ORM\ManyToOne(targetEntity="A2\StorehouseBundle\Entity\Storehouse", cascade={"persist"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $entrepot;
+
+    /**
+     * @ORM\Column(name="sellerNbVente", type="integer")
+     */
+    private $nbVente;
+
+    /**
      * @ORM\OneToOne(targetEntity="A2\UserBundle\Entity\User", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
      * @Assert\Valid()
      */
     private $user;
@@ -61,5 +73,53 @@ class Seller
     public function getUser()
     {
         return $this->user;
+    }
+
+    /**
+     * Set nbVente
+     *
+     * @param integer $nbVente
+     *
+     * @return Seller
+     */
+    public function setNbVente($nbVente)
+    {
+        $this->nbVente = $nbVente;
+
+        return $this;
+    }
+
+    /**
+     * Get nbVente
+     *
+     * @return integer
+     */
+    public function getNbVente()
+    {
+        return $this->nbVente;
+    }
+
+    /**
+     * Set entrepot
+     *
+     * @param \A2\StorehouseBundle\Entity\Storehouse $entrepot
+     *
+     * @return Seller
+     */
+    public function setEntrepot(\A2\StorehouseBundle\Entity\Storehouse $entrepot)
+    {
+        $this->entrepot = $entrepot;
+
+        return $this;
+    }
+
+    /**
+     * Get entrepot
+     *
+     * @return \A2\StorehouseBundle\Entity\Storehouse
+     */
+    public function getEntrepot()
+    {
+        return $this->entrepot;
     }
 }
