7bc04c4d43325ecc3c1e9acf0d8661ea333aad3d
A2lam <mohamedallam31@hotmail.com>
2017-12-12 02:44:58 +0100

    Maj getter setter entite User

--- src/A2/UserBundle/Entity/User.php
+++ src/A2/UserBundle/Entity/User.php
@@ -85,4 +85,244 @@ class User extends BaseUser
     {
         parent::__construct();
     }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return User
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return User
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set ddn
+     *
+     * @param \DateTime $ddn
+     *
+     * @return User
+     */
+    public function setDdn($ddn)
+    {
+        $this->ddn = $ddn;
+
+        return $this;
+    }
+
+    /**
+     * Get ddn
+     *
+     * @return \DateTime
+     */
+    public function getDdn()
+    {
+        return $this->ddn;
+    }
+
+    /**
+     * Set tel
+     *
+     * @param string $tel
+     *
+     * @return User
+     */
+    public function setTel($tel)
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    /**
+     * Get tel
+     *
+     * @return string
+     */
+    public function getTel()
+    {
+        return $this->tel;
+    }
+
+    /**
+     * Set adminCreation
+     *
+     * @param integer $adminCreation
+     *
+     * @return User
+     */
+    public function setAdminCreation($adminCreation)
+    {
+        $this->adminCreation = $adminCreation;
+
+        return $this;
+    }
+
+    /**
+     * Get adminCreation
+     *
+     * @return integer
+     */
+    public function getAdminCreation()
+    {
+        return $this->adminCreation;
+    }
+
+    /**
+     * Set dateCreation
+     *
+     * @param \DateTime $dateCreation
+     *
+     * @return User
+     */
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreation
+     *
+     * @return \DateTime
+     */
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }
+
+    /**
+     * Set utModification
+     *
+     * @param integer $utModification
+     *
+     * @return User
+     */
+    public function setUtModification($utModification)
+    {
+        $this->utModification = $utModification;
+
+        return $this;
+    }
+
+    /**
+     * Get utModification
+     *
+     * @return integer
+     */
+    public function getUtModification()
+    {
+        return $this->utModification;
+    }
+
+    /**
+     * Set dateModif
+     *
+     * @param \DateTime $dateModif
+     *
+     * @return User
+     */
+    public function setDateModif($dateModif)
+    {
+        $this->dateModif = $dateModif;
+
+        return $this;
+    }
+
+    /**
+     * Get dateModif
+     *
+     * @return \DateTime
+     */
+    public function getDateModif()
+    {
+        return $this->dateModif;
+    }
+
+    /**
+     * Set estActif
+     *
+     * @param string $estActif
+     *
+     * @return User
+     */
+    public function setEstActif($estActif)
+    {
+        $this->estActif = $estActif;
+
+        return $this;
+    }
+
+    /**
+     * Get estActif
+     *
+     * @return string
+     */
+    public function getEstActif()
+    {
+        return $this->estActif;
+    }
+
+    /**
+     * Set adresse
+     *
+     * @param \A2\AddressBundle\Entity\Address $adresse
+     *
+     * @return User
+     */
+    public function setAdresse(\A2\AddressBundle\Entity\Address $adresse = null)
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    /**
+     * Get adresse
+     *
+     * @return \A2\AddressBundle\Entity\Address
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
 }
