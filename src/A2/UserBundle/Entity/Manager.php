e5336310b3de06aa80d18efbb02a8bf77a9ce667
A2lam <mohamedallam31@hotmail.com>
2017-12-12 03:03:49 +0100

    Creation de l'entite Manager

---
+++ src/A2/UserBundle/Entity/Manager.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace A2\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Manager
+ *
+ * @ORM\Table(name="manager")
+ * @ORM\Entity(repositoryClass="A2\UserBundle\Repository\ManagerRepository")
+ */
+class Manager
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="A2\UserBundle\Entity\User", cascade={"persist", "remove"})
+     * @Assert\Valid()
+     */
+    private $user;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \A2\UserBundle\Entity\User $user
+     *
+     * @return Manager
+     */
+    public function setUser(\A2\UserBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \A2\UserBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
