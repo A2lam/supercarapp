2174bec7aaa6ca2fd46fcc57f288d1bbb25c7ab0
A2lam <mohamedallam31@hotmail.com>
2017-12-20 00:28:38 +0100

    Creation du constructeur de formulaire d'Address

---
+++ src/A2/AddressBundle/Form/AddressType.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Allam
+ * Date: 22/12/2017
+ * Time: 18:00
+ */
+
+namespace A2\AddressBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddressType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('streetNb',   IntegerType::class)
+            ->add('streetName', TextType::class)
+            ->add('complement', TextType::class)
+            ->add('zipCode',    IntegerType::class)
+            ->add('town',       TextType::class)
+            ->add('country',    TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'A2\AddressBundle\Entity\Address'
+        ));
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'a2_addressbundle_address';
+    }
+}
