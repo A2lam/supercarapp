af456e82a4ef74f5f79efcb300dff6eb68e3e9b4
A2lam <mohamedallam31@hotmail.com>
2017-12-20 01:33:32 +0100

    Creation du generateur de formulaire d'Image

---
+++ src/A2/ImageBundle/Form/ImageType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace A2\ImageBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ImageType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('file', FileType::class);
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'A2\ImageBundle\Entity\Image'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'a2_imagebundle_image';
+    }
+}
