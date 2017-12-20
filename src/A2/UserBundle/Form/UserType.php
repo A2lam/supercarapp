0ca715d6730e59cacef696ac5a032e59472a2c7e
A2lam <mohamedallam31@hotmail.com>
2017-12-20 01:38:25 +0100

    Completion du generateur de formulaire User

--- src/A2/UserBundle/Form/UserType.php
+++ src/A2/UserBundle/Form/UserType.php
@@ -2,7 +2,13 @@
 
 namespace A2\UserBundle\Form;
 
+use A2\AddressBundle\Form\AddressType;
+use A2\ImageBundle\Form\ImageType;
+use FOS\UserBundle\Form\Type\RegistrationFormType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +19,15 @@ class UserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')->add('lastname')->add('bday')->add('num')->add('adminAdd')->add('dateAdd')->add('userUpdate')->add('dateUpdate')->add('isActive')->add('image')->add('address');
+        $builder
+            ->add('name',     TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('bday',     DateTimeType::class)
+            ->add('num',      IntegerType::class)
+            ->add('address',  AddressType::class)
+            ->add('conInfos', RegistrationFormType::class)
+            ->add('image',    ImageType::class)
+        ;
     }/**
      * {@inheritdoc}
      */
@@ -31,6 +45,4 @@ class UserType extends AbstractType
     {
         return 'a2_userbundle_user';
     }
-
-
 }
