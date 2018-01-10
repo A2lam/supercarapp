992b48c995afd53acb6abe5a58e6dc99b2588e9d
A2lam <mohamedallam31@hotmail.com>
2018-01-10 22:21:19 +0100

    Creation de accueilAction dans le CoreController

--- src/CoreBundle/Controller/CoreController.php
+++ src/CoreBundle/Controller/CoreController.php
@@ -14,6 +14,74 @@ class CoreController extends Controller
 {
     public function accueilAction()
     {
-        return $this->render('CoreBundle:Core:accueil.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $modelsAReguler = array();
+        $qteTotal = 0;
+
+        // Verification de la nature de l'utilisateur connecté
+        // Pour la récuperation des ses notif personnelles
+        $roles = $this->getUser()->getRoles();
+        foreach ($roles as $role)
+        {
+            if ($role == "ROLE_ADMIN")
+            {
+                $models = $em
+                    ->getRepository('A2ModelBundle:Model')
+                    ->findByIsActive(true)
+                ;
+
+                foreach ($models as $model)
+                {
+                    // Recuperation de tous les stock concernes
+                    $stocks = $em
+                        ->getRepository('A2StockBundle:Stock')
+                        ->findByModel($model)
+                    ;
+
+                    foreach ($stocks as $stock)
+                    {
+                        $qteTotal += $stock->getQuantity();
+                    }
+
+                    if ($qteTotal < $model->getAlertValue())
+                        $modelsAReguler[] = $model;
+                }
+            }
+            elseif ($role == "ROLE_MANAGER")
+            {
+
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        // Recupération des recentes notif pour tous les utilisateurs
+        $sales = $em
+            ->getRepository('A2SaleBundle:Sale')
+            ->findBy(
+                array('isActive' => true),
+                array('dateAdd'  => 'DESC'),
+                3,
+                0
+            )
+        ;
+        $orders = $em
+            ->getRepository('OrderBundle:Orders')
+            ->findBy(
+                array('isActive' => true),
+                array('dateAdd'  => 'DESC'),
+                3,
+                0
+            )
+        ;
+
+        return $this->render('CoreBundle:Core:accueil.html.twig', array(
+            'sales'          => $sales,
+            'orders'         => $orders,
+            'modelsAReguler' => $modelsAReguler
+        ));
     }
 }
