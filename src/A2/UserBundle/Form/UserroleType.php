41f7efb54ce491286718315146575a44495657cb
A2lam <mohamedallam31@hotmail.com>
2018-01-11 21:05:41 +0100

    Creation du formulaire du choix du role lors de la creation utilisateur

---
+++ src/A2/UserBundle/Form/UserroleType.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Allam
+ * Date: 05/01/2018
+ * Time: 21:58
+ */
+
+namespace A2\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class UserroleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('role', ChoiceType::class, array(
+                'roles' => array(
+                    'Administrateur'  => 1,
+                    'Manager'         => 2,
+                    'Vendeur'         => 3
+                )
+            ))
+        ;
+    }
+}
